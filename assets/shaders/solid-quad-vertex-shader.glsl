543ac1cf274d205c17f25cd76351359cf814a085
thereclaimer <sam.davenport@thereclaimergame.com>
2024-04-27 20:21:18 -0500

    added the uniform obkject to the solid quad vertex shader

--- assets/shaders/solid-quad-vertex-shader.glsl
+++ assets/shaders/solid-quad-vertex-shader.glsl
@@ -1,9 +1,11 @@
 #version 330 core
 
 layout (location = 0) in vec2 in_vertex_position; 
-layout (location = 1) in vec4 in_vertex_color;    
 
-uniform mat4 model; 
+layout (std140) uniform solid_quad_update {
+    mat3 model;
+    vec4 color;
+};
 
 out vec4 out_vertex_color; 
 
@@ -11,5 +13,5 @@ void main()
 {
     gl_Position = model * vec4(in_vertex_position, 0.0, 1.0); 
     
-    out_vertex_color = in_vertex_color;
+    out_vertex_color = color;
 }
