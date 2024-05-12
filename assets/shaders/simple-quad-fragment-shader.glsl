4471f9d460fba848335c3b4da0071ac0f8a14aac
thereclaimer <sam.davenport@thereclaimergame.com>
2024-05-12 01:42:31 -0500

    updated the simple quad shader code

--- assets/shaders/simple-quad-fragment-shader.glsl
+++ assets/shaders/simple-quad-fragment-shader.glsl
@@ -1,10 +1,17 @@
 #version 330 core
 
 in vec4 fragment_color; 
+in vec2 fragment_uv;
+
+uniform sampler2D texture_sampler;
 
 out vec4 out_frag_color; 
 
 void main() {
 
-    out_frag_color = fragment_color; 
+    vec4 texture_color = texture(texture_sampler,fragment_uv);
+
+    out_frag_color = fragment_color.a > 0.0
+        ? fragment_color
+        : texture_color; 
 }
--- assets/shaders/simple-quad-vertex-shader.glsl
+++ assets/shaders/simple-quad-vertex-shader.glsl
@@ -1,15 +1,18 @@
 #version 330 core
 
 layout(location = 0) in vec2 vertex;
+layout(location = 1) in vec2 uv;
 
 uniform mat3 transform;
 uniform vec4 color;
 
 out vec4 fragment_color;
+out vec2 fragment_uv;
 
 void main() {
 
     gl_Position = vec4(transform * vec3(vertex, 1.0), 1.0);
 
     fragment_color = color;
+    fragment_uv    = uv;
 }
