ea96695c11c1a1aea4d497f57c20069dafeeba9e
Sam Davenport <sam@davenportdronographics.com>
2024-12-18 20:39:28 -0600

    changed scale from float to vector

--- math/ifb-math-mat3.hpp
+++ math/ifb-math-mat3.hpp
@@ -23,9 +23,9 @@ namespace ifb_math {
 
     void mat3_batch_identity    (const ifb_u32 count, ifb_m3* m3);
 
-    void mat3_batch_scale       (const ifb_u32 count, const ifb_f32* scale,    ifb_m3* m3);
-    void mat3_batch_translate   (const ifb_u32 count, const ifb_v3*  position, ifb_m3* m3);
-    void mat3_batch_rotate      (const ifb_u32 count, const ifb_v3*  axis,     ifb_m3* m3);
+    void mat3_batch_scale       (const ifb_u32 count, const ifb_v3* scale,    ifb_m3* m3);
+    void mat3_batch_translate   (const ifb_u32 count, const ifb_v3* position, ifb_m3* m3);
+    void mat3_batch_rotate      (const ifb_u32 count, const ifb_v3* axis,     ifb_m3* m3);
 
     void mat3_batch_a_mul_b     (const ifb_u32 count, const ifb_m3* m3_a, const ifb_m3* m3_b, ifb_m3* m3_c);
     
